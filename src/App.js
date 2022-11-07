9cf6d0937a26f84855da3c47b882034d28aa379d
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2022-11-07 13:25:25 +0200

    Commit #1.1
    Setup initial page routes and created an export file called index.js in the pages folder in order to store all the imports in it.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {Home,About,Cart,Error,CheckOut,Products,SingleProduct,PrivateRoute} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/cart' element={<Cart/>}/>
+        <Route exact path='/products' element={<Products/>}/>
+        <Route exact path='/products/:id' children={<SingleProduct/>}/>
+        <Route exact path='/checkout' element={<CheckOut/>}/>
+        <Route exact path='*' element={<Error/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import CheckOut from './CheckoutPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import PrivateRoute from './PrivateRoute'
+
+export{
+    Home,
+    About,
+    Cart,
+    Error,
+    CheckOut,
+    Products,
+    SingleProduct,
+    PrivateRoute
+}
