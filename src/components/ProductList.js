352c6ccd59dee921d15e6cd218ab39e284bace4b
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2022-11-12 13:00:37 +0200

    Commit #3.2
    *Developing Products Page (ProductsList)*
    - Added a gridView boolean in the filter_context in order to use it as a switch between grid and list view.
    - Finished setting up the GridView and ListView components that are located in ProductsList component.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,19 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  const {filteredProducts, gridView} = useFilterContext()
+
+  if(filteredProducts.length < 1){
+    return <h5 style={{textTransform: 'none'}}>
+      Sorry, no products matched your search...
+    </h5>
+  }
+
+  if(!gridView){
+    return <ListView products={filteredProducts}/>
+  }
+  
+  return <GridView products={filteredProducts}/>
 }
 
 export default ProductList
